1fa4c566a18fdde17ea0a46f9e6a10de8d3a6428
OMENZYC\13566 <yzhao6141@gmail.com>
2025-06-24 15:45:26 +0800

    1

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,20 @@
+<!-- src/components/MyName.vue -->
+<template>
+  <div class="my-name">
+    <h2>Hello, Yechong Zhao!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+}
+</script>
+
+<style scoped>
+.my-name {
+  margin-top: 20px;
+  font-size: 24px;
+  color: #2c3e50;
+}
+</style>
