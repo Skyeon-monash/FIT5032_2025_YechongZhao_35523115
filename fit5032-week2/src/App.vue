3d325966604657b4497e0aacd143da38eef92fb2
OMENZYC\13566 <yzhao6141@gmail.com>
2025-07-01 15:55:28 +0800

    week2

---
+++ fit5032-week2/src/App.vue
@@ -0,0 +1,61 @@
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style> -->
+
+<!-- template可以让其他文件直接import该文件 -->
+<template>
+  <div id="app">
+    <JSON /> <!-- 直接渲染 JSON.vue 组件 -->
+  </div>
+</template>
+
+<script>
+import JSON from './components/JSON.vue' // 引入组件
+export default {
+  components: { JSON }
+}
+</script>
